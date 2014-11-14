e1e3a05f8be658bb3e707c30b409c73a0d281553
fsxchen <fsxchen@gmail.com>
2014-11-14 17:55:13 +0800

    pyMQ study

--- pyMQConsum.py
+++ pyMQConsum.py
@@ -1,31 +1,67 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-import pika
-import pika, os, urlparse, logging
-logging.basicConfig()
+import pika, os, urlparse, json
+import socket
 
-# Parse CLODUAMQP_URL (fallback to localhost)
-url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://aaa:bbb@x.x.x./xxx')
-url = urlparse.urlparse(url_str)
-params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
-    credentials=pika.PlainCredentials(url.username, url.password))
+URL = ''
+TIMEOUT = 10
+INFODICT = {
+    "id": 0,
+    "ip": '',
+    "packet_id": 0,
+    "packet_size": 0,
+    "script_start_time": '',
+    "script_end_time": '',
+    "result": '',
+    "res_enctypes": 0,
+    "sent_time": '',
+    "recv_time": '',
+    "res_type": 0
+}
 
-connection = pika.BlockingConnection(params) # Connect to CloudAMQP
-channel = connection.channel() # start a channel
-# channel.queue_declare(queue='hello') # Declare a queue
-# send a message
-# channel.basic_publish(exchange='', routing_key='hello', body='Hello CloudAMQP!')
+socket.timeout = TIMEOUT
 
-# create a function which is called on incoming messages
+def connect2ser(url):
+    url_str = os.environ.get('CLOUDAMQP_URL', url)
+    url = urlparse.urlparse(url_str)
+    params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
+        credentials=pika.PlainCredentials(url.username, url.password))
+    connection = pika.BlockingConnection(params) # Connect to CloudAMQP
+    channel = connection.channel()
+    return channel
+
+def consum(channel, queueName='hello'):
+    channel.basic_consume(callback,queue=queueName,no_ack=True)
+    channel.start_consuming()
+
 def callback(ch, method, properties, body):
-  print " [x] Received %r" % (body)
+    print " [x] Received %r" % (body)
+    revd_info = json.loads(body)
+    if revd_info["id"] == 1:
+        ip = revd_info["ip"]
+        isAlive(ip, port)
 
-# set up subscription on the queue
-channel.basic_consume(callback,
-    queue='hello',
-    no_ack=True)
+def isAlive(ip, port):
+    HELLO = "hello\n"
+    try:
+        socket = socket.socket()
+        socket.connect((ip, port))
+        socket.send(HELLO)
+        rec_data = socket.recv(1024)
+        if rec_data.strip() == "OK":
+            return True
+    except Exception, e:
+        print e
+        return False
 
-channel.start_consuming() # start consuming (blocks)
 
-connection.close()
+def main():
+    ch = connect2ser(URL)
+    consum(ch)
+    print "hello coucou"
+
+isAlive("127.0.0.1", 80)
+
+# if __name__ == '__main__':
+#     main()
--- pyMQProduct.py
+++ pyMQProduct.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 #coding:utf-8
-import pika, os, urlparse, logging
-logging.basicConfig()
+import pika, os, urlparse
 
-# Parse CLODUAMQP_URL (fallback to localhost)
-url_str = os.environ.get('CLOUDAMQP_URL', 'amqp://amqp://aaa:bbb@x.x.x./xxx')
-url = urlparse.urlparse(url_str)
-params = pika.ConnectionParameters(host=url.hostname, virtual_host=url.path[1:],
-    credentials=pika.PlainCredentials(url.username, url.password))
+import json
 
-connection = pika.BlockingConnection(params) # Connect to CloudAMQP
-channel = connection.channel() # start a channel
-channel.queue_declare(queue='hello') # Declare a queue
-# send a message
-channel.basic_publish(exchange='', routing_key='hello', body='Hello 的大!')
-print " [x] Sent 'Hello World!'"
+test = {
+    "id": 1,
+    "ip": '192.168.1.254',
+    "packet_id": 60,
+    "packet_size": 0,
+    "script_start_time": '',
+    "script_end_time": '',
+    "result": '',
+    "res_enctypes": 0,
+    "sent_time": '',
+    "recv_time": '',
+    "res_type": 0
+}
+
+test_str = json.dumps(test)
+
+try:
+    eval(URL)
+except NameError, e:
+    from pyMQConsum import URL, connect2ser
+
+def sentMessage(ch, queueName="hello", body="hello amqp"):
+    ch.queue_declare(queue=queueName) 
+    ch.basic_publish(exchange='', routing_key=queueName, body=body)
+    print " [x] Sent 'Hello World!'"
+    ch.close()
+
+if __name__ == '__main__':
+    ch = connect2ser(URL)
+    sentMessage(ch, body=test_str)
